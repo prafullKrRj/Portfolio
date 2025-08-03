bb7dc6ae4e93f642f8cc94d44f462d03e682ba2c
prafullKrRj <prafullkumar384@gmail.com>
2025-08-04 01:44:46 +0530

    react

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Replace 'Portfolio' with your actual repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
